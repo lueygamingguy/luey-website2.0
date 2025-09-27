fe7fe5412241f4c6632513bd52d0fa213ddd5d81
lueygamingguy <luey_gaming_guy@yahoo.com>
2025-09-27 07:52:28 -0700

    Update navbar.js

--- navbar.js
+++ navbar.js
@@ -1,29 +1,7 @@
-// Navbar
-const navbarHTML = `
-<nav class="navbar">
-  <a href="index.html">Home</a>
-  <a href="shop.html">Shop</a>
-  <a href="custom-orders.html">Custom Orders</a>
-  <a href="content.html">Content</a>
-  <a href="contact.html">Contact</a>
-</nav>
-`;
-
-document.getElementById('navbar-placeholder').innerHTML = navbarHTML;
-
-// Footer
-const footerHTML = `
-<footer>
-  <p>&copy; 2025 LueyGamingGuy. All Rights Reserved.</p>
-  <p>
-    <a href="https://www.twitch.tv/luey_gaming_guy" target="_blank">Twitch</a> |
-    <a href="https://www.youtube.com/@luey_gaming_guy" target="_blank">YouTube</a> |
-    <a href="https://www.tiktok.com/@luey_gaming_guy" target="_blank">TikTok</a> |
-    <a href="https://www.instagram.com/luey_gaming_guy" target="_blank">Instagram</a> |
-    <a href="https://discord.com/invite/J9PaPxggd8" target="_blank">Discord</a> |
-    <a href="https://www.whatnot.com/user/luey_gaming_guy" target="_blank">Whatnot</a>
-  </p>
-</footer>
-`;
-
-document.getElementById('footer-placeholder').innerHTML = footerHTML;
+document.addEventListener('DOMContentLoaded', function() {
+          fetch('navbar.html')
+            .then(response => response.text())
+            .then(data => {
+              document.getElementById('navbar-placeholder').innerHTML = data;
+            });
+        });
